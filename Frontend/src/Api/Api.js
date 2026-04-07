62c5517e06304201a80322c6e6ffd1e0d4a3c642
kabhi2004 <abhishekkashyap2501@gmail.com>
2026-04-07 18:47:52 +0530

    Change Url

--- Frontend/src/Api/Api.js
+++ Frontend/src/Api/Api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "facial-recognition-attendance-system-production.up.railway.app";
 
 /* ---------- LOGIN ---------- */
 export async function loginUser(role, email, password) {
