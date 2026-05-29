ac1f0d26d3120f3260a489c8c78b5fd734d04e04
kabhi2004 <abhishekkashyap2501@gmail.com>
2026-05-29 16:08:30 +0530

    Change url

--- Frontend/src/Api/Api.js
+++ Frontend/src/Api/Api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://kabhi25-unicheck.hf.space";
 
 /* ---------- LOGIN ---------- */
 export async function loginUser(role, email, password) {
